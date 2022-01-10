027c8f2bcf559597ec0c30f3344e4a534cf89574
Marco Sterbik <madbadmax00@gmail.com>
2022-01-10 22:41:22 +0100

    begin transform into real mask folder

--- app/boards/arm/mask/board.cmake
+++ app/boards/arm/mask/board.cmake
@@ -1,8 +1,17 @@
-# Copyright (c) 2021 Nick Winans
-# Copyright (c) 2021 Marco Sterbik
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: Apache-2.0
 
-set(OPENOCD_NRF5_SUBFAMILY nrf52)
-board_runner_args(nrfjprog "--nrf-family=NRF53" "--softreset")
+if(CONFIG_BOARD_NRF5340PDK_NRF5340_CPUAPPNS OR CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPPNS)
+  set(TFM_PUBLIC_KEY_FORMAT "full")
+endif()
+
+if((CONFIG_BOARD_NRF5340PDK_NRF5340_CPUAPP OR CONFIG_BOARD_NRF5340PDK_NRF5340_CPUAPPNS) OR
+  (CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP OR CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPPNS))
+board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
+endif()
+
+if(CONFIG_BOARD_NRF5340PDK_NRF5340_CPUNET OR CONFIG_BOARD_NRF5340DK_NRF5340_CPUNET)
+board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
+endif()
+
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
