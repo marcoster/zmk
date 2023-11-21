a9a822e4e50683f2c760c7d07b9eb78069dadaa1
Marco Sterbik <madbadmax00@gmail.com>
2023-11-21 06:36:17 +0100

    klor: tinker around with the encoder

--- app/klor_build.sh
+++ app/klor_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 west build -b mask_module -d build_klor_left -- -DSHIELD=klor_left
-west build -b mask_module -d build_klor_right -- -DSHIELD=klor_right
 cp build_klor_left/zephyr/zmk.uf2 klor_left.uf2
+west build -b mask_module -d build_klor_right -- -DSHIELD=klor_right
 cp build_klor_right/zephyr/zmk.uf2 klor_right.uf2
